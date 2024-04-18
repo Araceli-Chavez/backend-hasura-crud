ec38163b914da4bf2fd112260c0737c4128c8893
Araceli6 <arachavez1901@gmail.com>
2024-04-18 16:20:40 -0600

    Corregi el valor de puerto en index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express  from 'express';
 import preactivaciontarjetasRouter from './routes/hasura.route.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
